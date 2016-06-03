9955fec987d00cafc2cea86a843c3cccc39a46d6
zengjun8485 <zengjun8485@163.com>
2016-06-03 16:50:15 +0800

    System.out.print("Browser")

--- src/com/emc/interview/fibonacci/Fibonacci.java
+++ src/com/emc/interview/fibonacci/Fibonacci.java
@@ -88,6 +88,7 @@ public class Fibonacci implements Iterable<BigInteger> {
 	 */
 	@Override
 	public Iterator<BigInteger> iterator() {
+		System.out.print("Browser")
 		return new FibonacciIterator(n);
 	}
 }
